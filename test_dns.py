53d1732b01f69e61e953cc96211dbbfa3cdb46d5
Kevin Kee <kevinkee9@gmail.com>
2020-04-09 03:49:31 +0000

    Clean up test_dns and use better assert, but still using static dummy DNS header

--- test_dns.py
+++ test_dns.py
@@ -2,8 +2,11 @@
 import pytest
 from modules.classes import Server, Query, Response
 
-class TestResponse:
 
-    def test_build_transaction_id(self, data = b'\xbb\xa6\x01 \x00\x01\x00\x00\x00\x00\x00\x01\x07howcode\x03org\x00\x00\x01\x00\x01\x00\x00)\x10\x00\x00\x00\x00\x00\x00\x00'):
-        assert type(data) is bytes
-        assert data[:2]
+def test_build_transaction_id():
+    raw_tid = b'\xbb\xa6\x01 \x00\x01\x00\x00\x00\x00\x00\x01' \
+        + b'\x07howcode\x03org\x00\x00\x01\x00\x01\x00\x00)' \
+        + b'\x10\x00\x00\x00\x00\x00\x00\x00'
+    r = Response()
+    r.build_tid(raw_tid)
+    #assert 'XXX' == r.tid
