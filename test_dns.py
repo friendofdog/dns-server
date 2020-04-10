90b09fc7867c86a01b6e5474224bac1eada0832d
Kevin Kee <kevinkee9@gmail.com>
2020-04-10 10:48:00 +0900

    Expand test for build tid, add test for build flags
    - Both tests verify data type, data length, and evaluation of data based on a dummy DNS call header
    - Deciding to use static dummy DNS call because tid is randomly generated, difficult to verify in a test

--- test_dns.py
+++ test_dns.py
@@ -1,12 +1,31 @@
-# content of dns.py
+# content of dns-server.py
 import pytest
 from modules.classes import Server, Query, Response
 
-
-def test_build_transaction_id():
-    raw_tid = b'\xbb\xa6\x01 \x00\x01\x00\x00\x00\x00\x00\x01' \
+# dig howcode.org @127.0.0.1 +noadflag
+query = b'\x88\xd0\x01\x00\x00\x01\x00\x00\x00\x00\x00\x01' \
         + b'\x07howcode\x03org\x00\x00\x01\x00\x01\x00\x00)' \
         + b'\x10\x00\x00\x00\x00\x00\x00\x00'
+
+tid = '88d0'
+
+# qr = 0, op = 0000, aa=0, tc = 0, rd = 1, ra = 0, z = 000, rcode = 0000
+flags = b'\x85\x00'
+
+
+def test_build_transaction_id():
+    q_tid = query[:2]
     r = Response()
-    r.build_tid(raw_tid)
-    #assert 'XXX' == r.tid
+    r.build_tid(q_tid)
+    assert type(r.tid) is str
+    assert len(r.tid) == 4
+    assert r.tid == tid
+
+
+def test_build_flags():
+    q_flags = query[2:4]
+    r = Response()
+    r.build_flags(q_flags)
+    assert type(r.flags) is bytes
+    assert len(r.flags) == 2
+    assert r.flags == flags
